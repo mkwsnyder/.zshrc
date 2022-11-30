867fbc55e8228f2286192ed90adabd6e8400c348
Mark Snyder <20092441+mkwsnyder@users.noreply.github.com>
2022-11-30 15:09:58 -0700

    update alias to add phone for scrcpy

--- .zsh/06-aliases.zsh
+++ .zsh/06-aliases.zsh
@@ -39,4 +39,5 @@ alias md-to-pdf="pandoc -o output.pdf"
 # alias copy="xclip -selection c" # used by piping into `copy` e.g. `cat README.md | copy` ONLY WORKS ON XORG
 alias copy="wl-copy" # requires wl-clipboard package, piping into it copies
 alias update-mirrors="sudo reflector --country US --protocol http,https --age 12 --sort rate -n 5 --save /etc/pacman.d/mirrorlist --verbose"
-alias gloomhaven="java -jar /run/media/mark/DATA/programs/GloomhavenHelper/ghh.jar"
+# alias gloomhaven="java -jar /run/media/mark/DATA/programs/GloomhavenHelper/ghh.jar"
+alias phone="scrcpy"
