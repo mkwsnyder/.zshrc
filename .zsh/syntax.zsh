891541a8d9949a88b84eded751a86415038f96df
Mark Snyder <20092441+mkwsnyder@users.noreply.github.com>
2023-03-30 16:12:50 -0600

    fix comment

--- .zsh/syntax.zsh
+++ .zsh/syntax.zsh
@@ -43,6 +43,6 @@
 # ZSH_HIGHLIGHT_STYLES[assign]='none'
 
 
-# if i get lost go here https://geoff.greer.fm/lscolors/
+# reference: https://geoff.greer.fm/lscolors/
 export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 export CLICOLOR=true
